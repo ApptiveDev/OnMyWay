3f2eef07ea1ca75915c4d873d69965e0ef3dfaca
Seogyeong Yun <gsm09157@naver.com>
2026-03-09 19:36:44 +0900

    Refactor(repository): UsersRepository 불필요한 조회 메서드 제거
    JpaRepository에서 기본적으로 제공하는 findById() 메서드가 이미 존재하여
    중복되는 findByid() 메서드를 제거하고 Repository를 단순화함.
    Spring Data JPA 기본 기능을 활용하도록 수정.

--- backend/src/main/java/_team/onmyway/repository/UsersRepository.java
+++ backend/src/main/java/_team/onmyway/repository/UsersRepository.java
@@ -4,5 +4,5 @@ import _team.onmyway.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
-    public Users findByid(Long id);
+
 }
