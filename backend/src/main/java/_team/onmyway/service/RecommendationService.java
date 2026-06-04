0b148ac8f1d25eab29eff2d71e97c290d0c253c5
scholar-star <cheve1224@pusan.ac.kr>
2026-06-04 23:07:13 +0900

    영업 시간이 없을 경우를 대비한 분기 처리(임시적)

--- backend/src/main/java/_team/onmyway/service/RecommendationService.java
+++ backend/src/main/java/_team/onmyway/service/RecommendationService.java
@@ -258,17 +258,29 @@ public class RecommendationService {
         return Flux.fromIterable(places)
                 .flatMap(place -> {
                     List<WorkingTime> placeWorkingTime = place.getWorkingTimes();
-                    WorkingTime workingTime = placeWorkingTime.get(day%7);
+                    if (placeWorkingTime.isEmpty()) {
+                        return imageService.getImageURL(place)
+                                .map(imageURL -> toPlaceRecommendationDTO(
+                                        place, lat, lng,
+                                        true,
+                                        null,
+                                        null,
+                                        imageURL
+                                ));
+                    }
+                    else {
+                        WorkingTime workingTime = placeWorkingTime.get(day%7);
 
-                    // 2. imageService.getImageURL(place)가 Mono<String>을 반환한다고 가정
-                    return imageService.getImageURL(place)
-                            .map(imageURL -> toPlaceRecommendationDTO(
-                                    place, lat, lng,
-                                    workingTime.isClosed(),
-                                    workingTime.getOpenTime(),
-                                    workingTime.getCloseTime(),
-                                    imageURL
-                            ));
+                        // 2. imageService.getImageURL(place)가 Mono<String>을 반환한다고 가정
+                        return imageService.getImageURL(place)
+                                .map(imageURL -> toPlaceRecommendationDTO(
+                                        place, lat, lng,
+                                        workingTime.isClosed(),
+                                        workingTime.getOpenTime(),
+                                        workingTime.getCloseTime(),
+                                        imageURL
+                                ));
+                    }
                 })
                 .collectList() // 3. 비동기로 생성된 DTO들을 다시 List로 모음
                 .map(placeInfos -> {
