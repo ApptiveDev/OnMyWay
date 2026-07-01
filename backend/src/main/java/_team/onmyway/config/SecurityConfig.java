cc55f9bb7ab277c9607eedc28d1410be2ae20c9a
scholar-star <cheve1224@pusan.ac.kr>
2026-07-01 15:07:14 +0900

    CORS 추가 설정

--- backend/src/main/java/_team/onmyway/config/SecurityConfig.java
+++ backend/src/main/java/_team/onmyway/config/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
 
         configuration.addAllowedOrigin("http://localhost:3000");
         configuration.addAllowedOrigin("http://localhost:5173"); // Vite 기본 포트
-        configuration.addAllowedOrigin("http://localhost:8081");
+        configuration.addAllowedOrigin("https://test.onmyway.cloud");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true); // 쿠키 사용(JWT refresh)
