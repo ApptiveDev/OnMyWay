5ebabed3aa5430a79ff934f69fcfb700a1b3ff5d
seunga <sseung551@pusan.ac.kr>
2026-07-08 11:46:08 +0900

    랜딩페이지 및 디자인 변경사항

--- frontend/ganeungil/components/Layout.jsx
+++ frontend/ganeungil/components/Layout.jsx
@@ -1,11 +1,10 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
+import ScaledFrame from "./ScaledFrame";
 
 export default function Layout() {
   return (
-    <div style={{ fontFamily: "'Noto Serif KR', serif" }}>
-      <Header />
+    <ScaledFrame>
       <Outlet />
-    </div>
+    </ScaledFrame>
   );
 }
