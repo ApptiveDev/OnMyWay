d91754dfba883d9eab29f90585e0fa55fc5695d6
scholar-star <cheve1224@pusan.ac.kr>
2026-07-09 20:59:55 +0900

    위도 경도 필수 입력 X

--- backend/src/main/java/_team/onmyway/service/SearchService.java
+++ backend/src/main/java/_team/onmyway/service/SearchService.java
@@ -69,10 +69,10 @@ public class SearchService {
                 if (nearbyPlaces.size() < 3) allPlaces.addAll(nearbyPlaces);
                 else allPlaces.addAll(nearbyPlaces.subList(0, 2));
 
-                allPlaces.sort((a,b) -> Double.compare(
-                        distanceService.distance(a.getLatitude(), a.getLongitude(), position.getLat(), position.getLon()),
-                        distanceService.distance(b.getLatitude(), b.getLongitude(), position.getLat(), position.getLon())
-                ));
+//                allPlaces.sort((a,b) -> Double.compare(
+//                        distanceService.distance(a.getLatitude(), a.getLongitude(), position.getLat(), position.getLon()),
+//                        distanceService.distance(b.getLatitude(), b.getLongitude(), position.getLat(), position.getLon())
+//                ));
                 return allPlaces;
             } catch (JsonProcessingException e) {
                 e.printStackTrace();
